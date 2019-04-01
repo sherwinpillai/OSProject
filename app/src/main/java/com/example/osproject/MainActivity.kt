7099ef73bdc707dc2102d21afbde1b15753cbcbd
Sherwin PIllai <sherylcorina@gmail.com>
2019-04-01 05:45:24 +0530

    Basic Android App

---
+++ app/src/main/java/com/example/osproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.osproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val InitialTextViewTranslationY = textView_Progress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+    }
+}
