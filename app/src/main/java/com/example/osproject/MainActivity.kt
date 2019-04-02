96a23e38f4c09de79bdd0a148fef65b1c3f58404
Sherwin PIllai <sherylcorina@gmail.com>
2019-04-02 06:23:05 +0530

    Final

--- app/src/main/java/com/example/osproject/MainActivity.kt
+++ app/src/main/java/com/example/osproject/MainActivity.kt
@@ -4,27 +4,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val InitialTextViewTranslationY = textView_Progress.translationY
-        // Hi I am Sherwin Pillai
 
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
 
-            }//fhdgfydbfasbfja
-            //hjafknsjj
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
-            }
 
-        })
     }
 }
